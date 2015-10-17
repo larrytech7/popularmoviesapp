a3968a1e71b3008051718495fa696cc3900a4282
larrytech7 <larryakah@gmail.com>
2015-10-17 22:51:18 +0100

    modified adapter to return trailer view

--- app/src/main/java/nanodegree/android/com/popularmoviesapp/adapter/MovietrailerAdapter.java
+++ app/src/main/java/nanodegree/android/com/popularmoviesapp/adapter/MovietrailerAdapter.java
@@ -1,6 +1,7 @@
 package nanodegree.android.com.popularmoviesapp.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -8,6 +9,7 @@ import android.widget.ArrayAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
+import nanodegree.android.com.popularmoviesapp.R;
 import nanodegree.android.com.popularmoviesapp.model.Trailer;
 
 /**
@@ -17,10 +19,12 @@ import nanodegree.android.com.popularmoviesapp.model.Trailer;
 public class MovietrailerAdapter extends ArrayAdapter<Trailer> {
 
     private List<Trailer> trailers;
+    private Context ctx;
 
     public MovietrailerAdapter(Context context, ArrayList<Trailer> list) {
         super(context, 0);
         this.trailers = list;
+        this.ctx = context;
     }
 
     @Override
@@ -30,7 +34,11 @@ public class MovietrailerAdapter extends ArrayAdapter<Trailer> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        if(convertView == null){
+            convertView = LayoutInflater.from(this.ctx).inflate(R.layout.trailer_layout, parent, false);
+        }
+        return convertView;
+
     }
 
     @Override
