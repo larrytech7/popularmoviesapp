22d4f8321441c261991595c088ac48ffe5f7f01f
larrytech7 <larryakah@gmail.com>
2015-10-19 15:54:58 +0100

    Added the reviewer layout

---
+++ app/src/main/java/nanodegree/android/com/popularmoviesapp/adapter/ReviewerAdapter.java
@@ -0,0 +1,8 @@
+package nanodegree.android.com.popularmoviesapp.adapter;
+
+/**
+ * Project Popularmoviesapp
+ * Created by Larry Akah on 10/19/15 2:31 PM.
+ */
+public class ReviewerAdapter {
+}
---
+++ app/src/main/java/nanodegree/android/com/popularmoviesapp/model/Reviewer.java
@@ -0,0 +1,8 @@
+package nanodegree.android.com.popularmoviesapp.model;
+
+/**
+ * Project Popularmoviesapp
+ * Created by Larry Akah on 10/19/15 2:27 PM.
+ */
+public class Reviewer {
+}
